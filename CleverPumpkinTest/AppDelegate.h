94b51305859cdb30cd09e11d8b360101970e8041
Artem Abramov <ar.abramov@gmail.com>
2014-01-16 08:24:37 +0400

    Initial Commit

---
+++ CleverPumpkinTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CleverPumpkinTest
+//
+//  Created by Artem Abramov on 1/16/14.
+//  Copyright (c) 2014 Artem Abramov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
